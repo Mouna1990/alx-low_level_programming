05ee007fcc7fb8b6b3b70c63f3e9751ac4b4248f
Mouna1990 <mmenouer11@gmail.com>
2023-04-20 10:40:47 -0700

    linux

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 int main(void)
 {
-const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+const char message[] = "and that piece of art is useful\" - Dora Korpar\, 2015-10-19";
 write(2, message, sizeof(message) - 1);
 return (1);
 }
