2b015b53522fc2c7d258416e69d913a8559a1324
Mouna1990 <mmenouer11@gmail.com>
2023-04-21 10:58:22 -0700

    comb of three num

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+* main -  all possible different combinations of three digits..
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+int num1, num2, num3;
+for (num1 = 0; num1 <= 8; num1++)
+{
+for (num2 = num1 + 1; num2 <= 9; num2++)
+{
+for (num3 = num1 + num2 + 1; num3 <= 9; num3++)
+{
+putchar('0' + num1);
+putchar('0' + num2);
+putchar('0' + num3);
+if (num1 != 8 || num2 != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
+
