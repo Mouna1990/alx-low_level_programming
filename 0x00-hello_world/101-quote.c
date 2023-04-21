fb4ff2802990839fdd5c71380e81e23cd8620044
Mouna1990 <mmenouer11@gmail.com>
2023-04-20 17:25:01 -0700

    unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,14 @@
 #include <unistd.h>
+#define STDERR_FILENO 2
+/**
+* main - linux.
+* Return: Always 0 (success) or 1 (failure)
+*/
 int main(void)
 {
-const char message[] = "and that piece of art is useful\" - Dora Korpar\, 2015-10-19";
-write(2, message, sizeof(message) - 1);
+char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+ssize_t len = 59; /* length of message including newline */
+if (write(STDERR_FILENO, msg, len) != len)
 return (1);
+return (0);
 }
