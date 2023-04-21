e4fab241361ac171c04fca6866ca1eab0d9adf55
Mouna1990 <mmenouer11@gmail.com>
2023-04-21 09:36:42 -0700

    all plh without e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,10 +6,11 @@
 int main(void)
 {
 char c;
-for (c = 'a'; c <= 'e'; c++)
-putchar(c);	
-for (c = 'a'; c >= 'q'; c++)
+for (c = 'a'; c <= 'z'; c++)
+{
+if (c != 'e' && c != 'q')
 putchar(c);
+}
 putchar('\n');
 return (0);
 }
