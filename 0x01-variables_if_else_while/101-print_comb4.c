ab8a7995d4bb262e9c48b47f94263ea836d449ad
Mouna1990 <mmenouer11@gmail.com>
2023-04-21 11:09:03 -0700

    comb for three nums

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,16 +6,16 @@
 int main(void)
 {
 int num1, num2, num3;
-for (num1 = 0; num1 <= 8; num1++)
+for (num1 = 0; num1 < 10; num1++)
 {
-for (num2 = num1 + 1; num2 <= 9; num2++)
+for (num2 = num1 + 1; num2 < 10; num2++)
 {
-for (num3 = num1 + num2 + 1; num3 <= 9; num3++)
+for (num3 = num2 + 1; num3 < 10; num3++)
 {
-putchar('0' + num1);
-putchar('0' + num2);
-putchar('0' + num3);
-if (num1 != 8 || num2 != 9)
+putchar(num1 + '0');
+putchar(num2 + '0');
+putchar(num3 + '0');
+if (num1 != 7 || num2 != 8 || num3 != 9)
 {
 putchar(',');
 putchar(' ');
@@ -26,4 +26,3 @@ putchar(' ');
 putchar('\n');
 return (0);
 }
-
