79c77ff8342834047bdbb73232a28a5213becde5
Mouna1990 <mmenouer11@gmail.com>
2023-04-21 09:50:58 -0700

    0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main -  the alphabet in lowercase.
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+putchar(i + '0');
+putchar('\n');
+return (0);
+}
