1d933e7c587efd0fb509565d4f036ebed6fdc03c
Mouna1990 <mmenouer11@gmail.com>
2023-04-25 05:06:35 -0700

    loweralphabet

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include <main.h>
+/**
+* main -  print alphabet if it's lower.
+* Return: Always 0 (success)
+*/
+int _islower(int c);
+{
+if (c <= 97 && c >= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+_putchar('\n');
