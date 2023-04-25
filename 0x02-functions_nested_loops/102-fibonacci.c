41e56f83aff3fed922899867a31f2b472765c155
Mouna1990 <mmenouer11@gmail.com>
2023-04-25 13:15:13 -0700

    102-fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,17 +5,21 @@
 */
 int main(void)
 {
-unsigned long long fib1 = 1;
-unsigned long long fib2 = 2;
-unsigned long long temp;
-int count;
-printf("%llu, %llu, ", fib1, fib2);
-for (count = 3; count <= 50; count++)
+int i = 0;
+long j = 1, k = 2;
+while (i < 50)
 {
-temp = fib1 + fib2;
-printf("%llu, ", temp);
-fib1 = fib2;
-fib2 = temp;
+if (i == 0)
+printf("%ld", j);
+else if (i == 1)
+printf("%ld", k);
+else
+{
+k += j;
+j = k - j;
+printf(", %ld", k);
+}
+++i;
 }
 printf("\n");
 return (0);
