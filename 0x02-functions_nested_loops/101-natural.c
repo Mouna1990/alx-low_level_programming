71114edb1ae39c1d3a7efb9efa051e3c2df13f98
Mouna1990 <mmenouer11@gmail.com>
2023-04-25 12:16:21 -0700

    the natural num bellow 10

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
-* main - all the natural numbers below 10 that are multiples of 3 or 5.
-* Return: Always 0.
+* main - print the sum of all multiples of 3 or 5 up to 1024.
+* Return: Always (success)
 */
-int main() {
+int main(void) 
+{
 int sum = 0; 
 for (int i = 0; i < 1024; i++) {
 if (i % 3 == 0 || i % 5 == 0) {
