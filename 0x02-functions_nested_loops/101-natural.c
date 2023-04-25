d755145eb1a2eb7811dd1ba73d15457387d68991
Mouna1990 <mmenouer11@gmail.com>
2023-04-25 12:25:39 -0700

    natural num bellow than 10

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,15 +3,16 @@
 * main - print the sum of all multiples of 3 or 5 up to 1024.
 * Return: Always (success)
 */
-int main(void) 
+int main(void)
 {
-int sum = 0; 
-for (int i = 0; i < 1024; i++) {
-if (i % 3 == 0 || i % 5 == 0) {
-sum += i; 
+int sum = 0;
+for (int i = 0; i < 1024; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+{
+sum += i;
 }
 }
 printf("The sum of multiples of 3 or 5 below 1024 is: %d\n", sum);
 return (0);
 }
-
