5951fe3d86b3effd968e8f6a07fac2eed08b9693
Mouna1990 <mmenouer11@gmail.com>
2023-04-25 04:07:12 -0700

    10 time a to z

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - prints the alphabet in lowercase 10 times
+*
+* Return: void
+*/
+void print_alphabet_x10(void)
+{
+int i;
+int j;
+for (i = 0; i < 10; i++)
+{
+for (j = 97; j <= 122; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
+}
+}
