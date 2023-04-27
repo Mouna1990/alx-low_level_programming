7f1d4befeb9e2f184eee0ea0abfb1e0287c53db9
Mouna1990 <mmenouer11@gmail.com>
2023-04-27 06:29:43 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*_isdigit - checks for a digit
+*@c:check cara
+* Return: Always 0.
+*/
+int _isdigit(int c)
+{
+if (c == 0 || c != 'a')
+return(1);
+else
+return(0);
+}
