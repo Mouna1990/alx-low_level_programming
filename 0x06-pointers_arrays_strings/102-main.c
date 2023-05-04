0e9f3af171b9314dcb05d9124a0f029ca7aa4fdf
Mouna1990 <mmenouer11@gmail.com>
2023-05-04 09:58:28 -0700

    102-magic.c

---
+++ 0x06-pointers_arrays_strings/102-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - This program demonstrates how to modify
+* a variable indirectly using a pointer
+*/
+int main(void)
+{
+int n;
+int a[5];
+int *p;
+
+a[2] = 1024;
+p = &n;
+;
+printf("a[2] = %d\n", a[2]);
+return (0);
+}
