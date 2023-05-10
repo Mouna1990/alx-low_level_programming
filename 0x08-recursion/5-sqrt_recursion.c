01bb4e65ee2992bfa9d878e7d8f11b7115d4d543
Mouna1990 <mmenouer11@gmail.com>
2023-05-10 15:00:43 -0700

    squar of num

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,9 +8,9 @@
 */
 int _sqrt_recursion_helper(int n, int i)
 {
-while (i * i > n)
+if (i * i > n)
 return (-1);
-while (i * i == n)
+if (i * i == n)
 return (i);
 return (_sqrt_recursion_helper(n, i + 1));
 }
@@ -21,7 +21,7 @@ return (_sqrt_recursion_helper(n, i + 1));
 */
 int _sqrt_recursion(int n)
 {
-while (n < 0)
+if (n < 0)
 return (-1);
 return (_sqrt_recursion_helper(n, 0));
 }
