df03ac1ce0b3cf575b31f40ae2642033ad4a4ce2
Mouna1990 <mmenouer11@gmail.com>
2023-05-12 02:39:16 -0700

    static librarirs

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _puts - prints a string to stdout
+* @s: the string to print
+*
+* Return: void
+*/
+void _puts(char *s)
+{
+while (*s)
+_putchar(*s++);
+_putchar('\n');
+}
