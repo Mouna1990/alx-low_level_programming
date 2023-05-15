1ca419ee722f974f970453179469f70e0f0fe47d
Mouna1990 <mmenouer11@gmail.com>
2023-05-15 11:30:16 -0700

    argstostr

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+
+/**
+* argstostr - Concatenates all the arguments of the program.
+* @ac: Number of arguments.
+* @av: Array of arguments.
+*
+* Return: Pointer to the new string, or NULL if it fails.
+*/
+char *argstostr(int ac, char **av)
+{
+char *str;
+int i; 
+int length;
+int total_length = 0;
+int k = 0;
+
+if (ac == 0 || av == NULL)
+return (NULL);
+for (i = 0; i < ac; i++)
+{
+length = 0;
+while (av[i][length] != '\0')
+{
+length++;
+total_length++;
+}
+total_length++;
+}
+str = malloc(sizeof(char) * (total_length + 1));
+if (str == NULL)
+return (NULL);
+
+for (i = 0; i < ac; i++)
+{
+length = 0;
+while (av[i][length] != '\0')
+{
+str[k] = av[i][length];
+length++;
+k++;
+}
+str[k] = '\n';
+k++;
+}
+str[k] = '\0';
+
+return (str);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -6,4 +6,7 @@ void free_grid(int **grid, int height);
 char *str_concat(char *s1, char *s2);
 char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
+
 #endif
