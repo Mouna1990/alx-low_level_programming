a02402ad61af4e160100d3a657fc395d0c7840e5
mouna <mmenouer11@gmail.com>
2023-06-01 05:49:24 -0700

    linklist

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,20 @@
+section .data
+hello db 'Hello, Holberton', 0
+format db "%s", 0
+
+section .text
+extern printf
+
+global main
+main:
+push rbp
+mov rbp, rsp
+
+mov rdi, format
+mov rsi, hello
+xor eax, eax
+call printf
+
+mov eax, 0
+leave
+ret
